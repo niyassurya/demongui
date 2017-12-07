e0866942b35effaef7dddbea7b6924294b83f868
Niyas <niyassurya@gmail.com>
2017-12-07 10:50:21 +0530

    HTTP service changes

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -6,11 +6,16 @@ import { LoginRoutingModule } from './login-routing.module';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 import { ResetpasswordComponent } from './resetpassword/resetpassword.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   imports: [
     CommonModule,
     LoginRoutingModule,
+    FormsModule,
+    HttpModule,
+    ReactiveFormsModule,
     CustomerModule
   ],
   exports:[
--- src/app/login/signin/signin.component.ts
+++ src/app/login/signin/signin.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import {Http, Response, Request, Headers, RequestMethod, RequestOptions, RequestOptionsArgs} from '@angular/http';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-signin',
@@ -7,20 +10,60 @@ import { Router } from '@angular/router';
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
+  loginForm : FormGroup;
+  authenticated: boolean;
 
-  constructor(private router:Router) { }
+  constructor(private router:Router,fb: FormBuilder, public http: Http){
+    // if(localStorage.getItem('jwt')){
+    //   this.authenticated = true;
+    //   this.profile = JSON.parse(localStorage.getItem('profile'));
+    // }
+    this.loginForm = fb.group({
+      'email' : [null, Validators.required],
+      'password': [null, Validators.required],
+    })
+  }
 
   ngOnInit() {
   }
+  submitForm(value: any){
+    console.log('email -> ' + value.email + ' | password -> ' + value.password );
+    let form = {
+      'userName' : value.email,
+      'password' : value.password
+    }
 
-  loginUser(e) {
-  	e.preventDefault();
-  	console.log(e);
-  	var username = e.target.elements[0].value;
-  	var password = e.target.elements[1].value;
-  	console.log(username + '   ' + password);
-  	if(username == 'admin' && password == 'admin') {
-  		this.router.navigate(['customer']);
-  	}
+    // let headersObj  = new Headers({ 'Content-Type': 'application/json; charset=UTF-8' 
+    // ,'Access-Control-Allow-Origin': 'http://localhost:4200/'
+    // ,'Access-Control-Allow-Methods':'POST,GET,PUT,DELETE,OPTIONS'
+    // , 'Access-Control-Allow-Headers': 'Authorization,Lang' });
+
+    // let requestArg: RequestOptionsArgs = {  method: "POST", headers: headersObj  };
+    // let options = new RequestOptions( requestArg );
+
+    // this.http.post('http://localhost:8080/demo/login', form, options).subscribe(
+    this.http.post('http://localhost:8080/demo/login', form).subscribe(
+      (res:any)=>{
+        console.log('response -> ' + res);
+        console.log('response status -> ' + res.status);
+        console.log('response ok -> ' + res.ok);
+        console.log('headers -> ' + res.headers);
+        console.log('authorization -> ' + res.headers.get('Authorization'));        
+        console.log('headers -> ' + res.headers.get('cache-control'));
+        if(res.ok){
+          if(res.headers.get('Authorization')){
+            localStorage.setItem('jwt',  res.headers.get('Authorization'));
+            // this.getUserInfo(data);
+            this.router.navigate(['customer']);
+          }
+          else{
+
+          }
+        }
+        else{
+          
+        }
+      }
+    )
   }
 }
